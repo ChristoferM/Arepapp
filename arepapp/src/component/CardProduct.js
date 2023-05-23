3a7fe58d7f837421a6fef0fb4767bf1d4a55a739
juanvalgreen <gabroval@hotmail.com>
2023-05-22 22:32:31 -0500

    Fixing some styles

--- arepapp/src/component/CardProduct.js
+++ arepapp/src/component/CardProduct.js
@@ -15,19 +15,17 @@ function CardProduct({product, handleDetails}){
     return(
 
         <>
-            <Card className='my-3 rounded-4' >
+            <Card className='my-3 rounded-4 shadow-lg' >
                 <Card.Body className='d-flex align-items-center'>
-                    <div className='w-25 me-3' style={{ display: 'flex'}}>
-                        <Image className='h-50 w-100 ' src={product.imgUrl} alt={`Imagen de ${product.name}`} onClick={handleDetailsClick} rounded/>
-                    </div>
                     <div>
+                    <Card.Img className='mb-3' src={product.imgUrl} alt={`Imagen de ${product.name}`} onClick={handleDetailsClick} rounded/>
                     
                         <Card.Title>{product.name}</Card.Title>
                         <Card.Text> $ {product.price} </Card.Text>
                         <Card.Text className='fs-6 text-secondary fw-light'> {product.description.length > 35 ? product.description.substring(0, 35) + '...' : product.description}</Card.Text>
                         <ButtonGroup className='align-items-center'>
                             <Button  className='text-black' style={{backgroundColor: '#FEC151',border:'none'}}>+</Button>
-                            <span className="mx-3 fw-bolder" style={{ display: 'inline-block', verticalAlign: 'middle' }}>0</span>
+                            <span className="mx-2 fw-bolder" style={{ display: 'inline-block', verticalAlign: 'middle' }}>0</span>
                             <Button  className='text-black' style={{backgroundColor: '#FEC151',border:'none'}}>-</Button>
                         </ButtonGroup>
                         <Button className='mx-3 text-black fw-semibolder' style={{backgroundColor: '#FEC151',border:'none'}}>Eliminar</Button>
